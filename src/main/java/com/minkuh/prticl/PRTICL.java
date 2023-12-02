9493d609260db480fc73a977b2348e11557d3943
Minkuh <michalkralovic2@gmail.com>
2023-12-02 12:13:46 +0100

    Initial commit

---
+++ src/main/java/com/minkuh/prticl/PRTICL.java
@@ -0,0 +1,17 @@
+package com.minkuh.prticl;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class PRTICL extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
